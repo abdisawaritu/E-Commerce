d04fa073eb1c3c66ff045b904166bdaefc4b3bc2
Abdisa Waritu Ebiso <abdisawaritu4@gmail.com>
2026-08-26 02:46:33 -0700

    Render products dynamically using Dom manipulation

--- Js/script.js
+++ Js/script.js
@@ -38,5 +38,50 @@ let products = [
 
 let productsContainer = document.getElementById("products-container");
 
-console.log(products);
-console.log(productsContainer);
+for (let product of products) {
+  let productColumn = document.createElement("div");
+
+  productColumn.className = "col-12 col-sm-6 col-lg-4";
+
+  let productCard = document.createElement("div");
+
+  productCard.className = "product-card";
+
+  let productImage = document.createElement("img");
+
+  productImage.className = "product-image";
+
+  productImage.src = product.img;
+
+  productImage.alt = product.name;
+
+  let productContent = document.createElement("div");
+
+  productContent.className = "product-content";
+
+  let productName = document.createElement("h2");
+
+  productName.className = "product-name";
+
+  productName.textContent = product.name;
+
+  let productPrice = document.createElement("p");
+
+  productPrice.className = "product-price";
+
+  productPrice.textContent = `$${product.price}`;
+
+  let productButton = document.createElement("button");
+
+  productButton.className = "product-button";
+
+  productButton.textContent = "Add to Cart";
+
+  productContent.append(productName, productPrice, productButton);
+
+  productCard.append(productImage, productContent);
+
+  productColumn.append(productCard);
+
+  productsContainer.append(productColumn);
+}
