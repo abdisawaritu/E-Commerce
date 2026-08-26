d7ddfee9725415928bf912e48808ec2333e18115
Abdisa Waritu Ebiso <abdisawaritu4@gmail.com>
2026-08-26 02:06:12 -0700

    Add product data Array

---
+++ Js/script.js
@@ -0,0 +1,39 @@
+let products = [
+  {
+    name: "Laptop",
+    price: 850,
+    img: "./images/laptop.jpg",
+  },
+
+  {
+    name: "Smartphone",
+    price: 550,
+    img: "./images/smartphone.jpg",
+  },
+
+  {
+    name: "Headphones",
+    price: 120,
+    img: "./images/headphones.jpg",
+  },
+
+  {
+    name: "Smart Watch",
+    price: 180,
+    img: "./images/smart-watch.jpg",
+  },
+
+  {
+    name: "Digital Camera",
+    price: 700,
+    img: "./images/camera.jpg",
+  },
+
+  {
+    name: "Keyboard",
+    price: 75,
+    img: "./images/keyboard.jpg",
+  },
+];
+
+console.log(products);
